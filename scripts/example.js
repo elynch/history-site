5ce75ce76aebfd1f64b6b15b1e70e18bdcbe4ab7
ellenclynch@gmail.com <ellenlynch@ellens-mbp.attlocal.net>
2015-10-04 23:19:24 -0400

    Code for website
    Updating code for new website

---
+++ scripts/example.js
@@ -0,0 +1,15 @@
+
+
+$(function(){
+
+	var images = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"];
+	$('.container').css({'background-image': 'url(img/' + images[Math.floor(Math.random() * images.length)] + ')'});
+
+
+});
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+    	slideshow: false
+    });
+  });
